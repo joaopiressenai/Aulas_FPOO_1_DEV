f30e1e9371f57cfb1deaad6f5bd1dd9a06cac824
Aluno <joao.pires3@aluno.senai.br>
2023-07-28 10:48:44 -0300

    Criando variaveis em Java

--- Fundamento/src/fundamento/Fundamento.java
+++ Fundamento/src/fundamento/Fundamento.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package fundamento;
 
-/**
- *
- * @author Aluno
- */
 public class Fundamento {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Declaração de variáveis
+        String nome = "Moisés";
+        int idade = 40;
+        boolean estudando = true;
+        double media = 5.5;
+        
     }
     
 }
