98a55e0b26006c1bca7de9f611cc600fe5021601
Aluno <joao.pires3@aluno.senai.br>
2023-07-28 10:58:08 -0300

    Declarando string como objeto

--- Fundamento/src/fundamento/Fundamento.java
+++ Fundamento/src/fundamento/Fundamento.java
@@ -7,12 +7,13 @@ public class Fundamento {
      */
     public static void main(String[] args) {
         
-        // Declaração de variáveis
+        // Declaração de variáveis de tipos primitivos
         String nome = "Moisés";
         int idade = 40;
         boolean estudando = true;
         double media = 5.5;
         
+        String bairro = new String("Centro");   
     }
     
 }
