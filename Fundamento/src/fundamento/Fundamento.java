ed4d7e5488df532565a01252e0d0588a3f7c1f7e
Aluno <joao.pires3@aluno.senai.br>
2023-07-28 10:26:38 -0300

    Enviando arquivos do projeto de fundamentos em Java

---
+++ Fundamento/src/fundamento/Fundamento.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fundamento;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Fundamento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
