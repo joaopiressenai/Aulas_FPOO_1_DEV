a94308bf85f55e3d47d7b8d8e544e88888b9c32f
Aluno <joao.pires3@aluno.senai.br>
2023-07-28 11:19:34 -0300

    Aprendendo var args

---
+++ Fundamento/src/fundamento/MultiplasNotas.java
@@ -0,0 +1,19 @@
+package fundamento;
+
+public class MultiplasNotas {
+    public static void main(String[] args) {
+        int rs = somar(4, 8, 2, 1);
+        System.out.println("A soma: " + rs);
+    }
+    
+    public static int somar(int ... valores) {
+        int resultado = 0;
+        
+        for (int i = 0; i < valores.length; i++) {
+            int valor = valores[i];
+            resultado += valor;            
+        }
+        
+        return resultado;
+    }
+}
