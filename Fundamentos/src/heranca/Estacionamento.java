e352eec6474915a62fe7bdce00aff7075c2080aa
joaopiressenai <140816716+joaopiressenai@users.noreply.github.com>
2023-09-21 16:51:19 -0300

    Aprendendo herança

--- Fundamentos/src/heranca/Estacionamento.java
+++ Fundamentos/src/heranca/Estacionamento.java
@@ -13,5 +13,16 @@ public class Estacionamento {
         Carro gol = new Carro("Gol muito conservado", 1999, "Azul", 
                 "G2", "Volkswagem", 13999, false, false, 
                 5, CambioEnum.MANUAL, CombustivelEnum.GASOLINA);
+        System.out.println(gol);
+        System.out.println("------------------------------------------------------------");
+        Moto hornet = new Moto("Moto hornet 600 Cilindradas", 2014, "Preta",
+                "Hornet", "Honda", 40000, true, true,
+                CombustivelEnum.GASOLINA, PartidaEnum.ELETRICA);
+        System.out.println(hornet);
+        System.out.println("------------------------------------------------------------");
+        Scooter scooter = new Scooter(2200, "Scooter elétrica", 2023,
+                "Cinza", "ELEKTRA", "Honda", 10000, true, false);
+        System.out.println(scooter);
+        System.out.println("------------------------------------------------------------");
     }
 }
